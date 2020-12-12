eef9aa7acce5787c7cd675da4ae26fb18766428b
Velibor <veliborvasiljevic7@gmail.com>
2020-12-12 14:54:13 +0100

    cleared items and spider;

--- rottentomatoes/items.py
+++ rottentomatoes/items.py
@@ -8,10 +8,7 @@ import scrapy
 
 class MovieItem(scrapy.Item):
     name = scrapy.Field()
-    flag = scrapy.Field() #rating on website should be named flag
     genre = scrapy.Field()
-    original_language = scrapy.Field()
-    release_date = scrapy.Field()
-    # runtime = scrapy.Field()
+    consensus = scrapy.Field()
     tomatometer = scrapy.Field() # percent and total count
     
--- rottentomatoes/spiders/movies_spider.py
+++ rottentomatoes/spiders/movies_spider.py
@@ -8,55 +8,24 @@ class MovieSpider(scrapy.Spider):
         'https://www.rottentomatoes.com/top/bestofrt/?year=2020'
     ]
 
-    # this ll be start urls
+    # this'll be start urls
     # start_urls = [
         # f'https://www.rottentomatoes.com/top/bestofrt/?year={i}' for i in reversed(range(1990, 2021))
     # ]
 
 
     def parse(self, response):
-        # movie_links = response.css('#top_movies_main .articleLink::attr(href)').extract()
-
         for href in response.css('#top_movies_main .articleLink::attr(href)'):
             url = response.urljoin(href.extract())
-            # pogadjamo dobar url
-            print(f"Before yield on page: {url}")
+            print(f"Page: {url}")
             yield scrapy.Request(url, callback = self.parse_individual_movie)
+
 
     def parse_individual_movie(self, response):
         item = MovieItem()
         item['name'] = response.css('.mop-ratings-wrap__title--top::text').get()
-        item['flag'] = response.css('.clearfix:nth-child(1) .meta-value::text').get().replace('\n', '').strip()
         item['genre'] = response.css('.genre::text').get().replace(' ', '').replace('\n', '').replace('and', ',').split(',')
-        item['original_language'] = response.css('.clearfix:nth-child(3) .meta-value::text').get().replace('\n', '').strip()
-        item['release_date'] = response.css('.clearfix:nth-child(7) time::text').get()
-
-        # runtime = response.css('.clearfix:nth-child(10) time::text').get()
-
-
-        # item['runtime'] = response.css('.clearfix:nth-child(10) time::text').get().strip()
+        item['consensus'] = None # response.css('.genre::text').get().replace(' ', '').replace('\n', '').replace('and', ',').split(',')
         item['tomatometer'] = response.css('#tomato_meter_link .mop-ratings-wrap__percentage::text').get().strip()
 
         yield item
-
-    # shouldnt use this for every string    
-    def clear_string(self, string) -> str:
-        # za obican string
-        string = string.replace('\n', '')
-        string = string.replace(' ', '')
-
-        return string
-
-    def clear_list(self, string, delimeter: str) -> list:
-        string = self.clear_string(string)
-        return string.split(delimeter)
-
-
-
-
-
-
-
-            
-
-
