75275a354202e8712130ac36688cce41113e9c02
Velibor <veliborvasiljevic7@gmail.com>
2020-12-12 18:53:15 +0100

    consensus will not be scraped

--- rottentomatoes/spiders/movies_spider.py
+++ rottentomatoes/spiders/movies_spider.py
@@ -20,12 +20,11 @@ class MovieSpider(scrapy.Spider):
             print(f"Page: {url}")
             yield scrapy.Request(url, callback = self.parse_individual_movie)
 
-
+    
     def parse_individual_movie(self, response):
         item = MovieItem()
         item['name'] = response.css('.mop-ratings-wrap__title--top::text').get()
         item['genre'] = response.css('.genre::text').get().replace(' ', '').replace('\n', '').replace('and', ',').split(',')
         item['tomatometer'] = response.css('#tomato_meter_link .mop-ratings-wrap__percentage::text').get().strip()
         item['audience_score'] = response.css('.audience-score .mop-ratings-wrap__percentage::text').get().strip()
-
         yield item
