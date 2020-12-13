c1e351c3e3c1f880be818c815496a31fb5ba024c
Velibor <veliborvasiljevic7@gmail.com>
2020-12-13 17:39:05 +0100

    doneeee, scraped all the movies from 1990 - 2020 :fire:

--- rottentomatoes/spiders/movies_spider.py
+++ rottentomatoes/spiders/movies_spider.py
@@ -4,14 +4,14 @@ from ..items import MovieItem
 class MovieSpider(scrapy.Spider):
     name = 'movies'  # unique
 
-    start_urls = [
-        'https://www.rottentomatoes.com/top/bestofrt/?year=2020'
-    ]
+    # start_urls = [
+        # 'https://www.rottentomatoes.com/top/bestofrt/?year=2020'
+    # ]
 
     # this'll be start urls
-    # start_urls = [
-        # f'https://www.rottentomatoes.com/top/bestofrt/?year={i}' for i in reversed(range(1990, 2021))
-    # ]
+    start_urls = [
+        f'https://www.rottentomatoes.com/top/bestofrt/?year={i}' for i in reversed(range(1990, 2021))
+    ]
 
 
     def parse(self, response):
@@ -27,5 +27,11 @@ class MovieSpider(scrapy.Spider):
         item['genre'] = response.css('.genre::text').get().replace(' ', '').replace('\n', '').replace('and', ',').split(',')
         item['year'] = response.css('time::text').get().split(',')[-1].strip() # get gets the first <time>, which is always release date
         item['tomatometer'] = response.css('#tomato_meter_link .mop-ratings-wrap__percentage::text').get().strip()
-        item['audience_score'] = response.css('.audience-score .mop-ratings-wrap__percentage::text').get().strip()
+        
+        audience_score = response.css('.audience-score .mop-ratings-wrap__percentage::text').get()
+        if audience_score is not None:
+            item['audience_score'] = audience_score.strip()
+        else: 
+            item['audience_score'] = None
+
         yield item
